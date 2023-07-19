f6e6467b3a2778cde3f6235caf6ee29654b78518
tajay campbell <flarebrady@gmail.com>
2023-07-19 14:33:40 -0400

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from Config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-
-
-from app import routes
+# Must be at bottom of file prevent circular import
+from app import routes, models
